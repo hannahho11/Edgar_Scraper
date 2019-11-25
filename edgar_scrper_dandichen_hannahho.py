b8b9f362a63b84d9d1e19a399d78798b2ff80ed4
hnnhho <50061961+hnnhho@users.noreply.github.com>
2019-11-24 22:16:14 -0600

    Debugged, able to replicate results with range 20160101 to 20170101 for AMZN

--- edgar_scrper_dandichen_hannahho.py
+++ edgar_scrper_dandichen_hannahho.py
@@ -11,6 +11,7 @@ import urllib.request
 from IPython.display import display_html
 import re
 
+#test with date range 20160101 to 20170101
 
 #returns the company's CIK
 def get_ticker():
@@ -20,10 +21,10 @@ def get_ticker():
         if comp == '':
             comp = False
         else:
-            #get the search page 
+            #get the search page
             #count = 5 so parse fast since we are just getting the name of company
             baselink = 'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={}&type=&dateb=&owner=exclude&count=5'
-            #search lowercase 
+            #search lowercase
             comp = comp.lower()
             url = baselink.format(comp)
             page = requests.get(url)
@@ -49,14 +50,26 @@ def get_ticker():
                 right_company = False
                 while right_company == False:
                     print('\n\n')
-                    print('Is this the company you are looking for? (Press Y for yes N for no)')
+                    print('Is this the company you are looking for? (Press \'y\' or \'Enter\' for yes, \'n\' for no)')
                     right_company = input(comp_name)
-                    if right_company.upper() != 'Y' and right_company.upper() !='N':
-                        right_company = False
-                    elif right_company.upper() == 'N':
-                        comp = False
-                    else: 
+
+                    if right_company.lower() == 'y' or right_company == '':
+                        right_company = True
+                        comp = True
                         return comp_name.split(' ')[-2]
+
+                    if right_company.lower() == 'n':
+                        continue #GET THIS TO LOOP BACK and ask for ticker again
+                    else:
+                        print('Enter valid input')
+                        continue
+
+                    # if right_company.upper() != 'Y' or right_company.upper() !='N':
+                    #     right_company = False
+                    # elif right_company.upper() == 'N':
+                    #     comp = False
+                    # else:
+                    #     return comp_name.split(' ')[-2]
 
 #check if the date entered is valid
 #returns either False or valid date as pair [start,end]
@@ -64,9 +77,17 @@ def validate_start_date(start, end, earliest_date):
 
     current_date = int(date.today().strftime('%Y%m%d'))
 
+    #if start empty, place earliest available date
     if start == '':
         print ('Start date is', earliest_date)
-        return current_date
+        return earliest_date
+
+        #check if user enters anything other than numbers
+    try:
+        start_test = pd.to_datetime(start,format='%Y%m%d')
+    except:
+        print('Please enter numbers only. Date format: YYYYMMDD')
+        return False
 
     if int(start) > current_date:
         print('Please enter a start date after the end date.')
@@ -76,28 +97,30 @@ def validate_start_date(start, end, earliest_date):
         print ('Start date prior to May 2009. Dates prior to May 2009 are not supported.')
         return False
 
-    #if start empty, place earliest available date
-
     else:
         return start
-
-    #check if user enters anything other than numbers
-    try:
-        start_test = pd.to_datetime(start,format='%Y%m%d')
-    except:
-        print('Please enter numbers only. Date format: YYYYMMDD')
-        return False
 
     if len(start) != 8:
         print('Please check date format: YYYYMMDD')
         return False
 
-def validate_end_date(start_ok, end, earliest_date):
+def validate_end_date(start, end, earliest_date):
 
     current_date = int(date.today().strftime('%Y%m%d'))
 
     #if end empty or greater than current date, place current date
-    if end == '' or int(end) > current_date:
+    if end == '':
+        print ('End date is', current_date)
+        return current_date
+
+    #check if user enters anything other than numbers
+    try:
+        end_test = pd.to_datetime(end,format='%Y%m%d')
+    except:
+        print('Please enter numbers only. Date format: YYYYMMDD')
+        return False
+
+    if int(end) > current_date:
         print ('End date is', current_date)
         return current_date
 
@@ -109,16 +132,13 @@ def validate_end_date(start_ok, end, earliest_date):
         print('Please enter end date after the start date.')
         return False
 
-    #check if user enters anything other than numbers
-    try:
-        end_test = pd.to_datetime(end,format='%Y%m%d')
-    except:
-        print('Please enter numbers only. Date format: YYYYMMDD')
-        return False
+    else:
+        return end
 
     if len(end)!= 8:
         print('Please check date format: YYYYMMDD')
         return False
+
 
 #asking for the end year start year
 #return the final search range. format: list[startdate, enddate]
@@ -130,26 +150,29 @@ def get_years():
     valid_start_year = validate_start_date(start_year_input, end_year_input, earliest_date) #stores either False or a valid_start_year
 
     while valid_start_year == False: #if start date invalid, repeat until proper start date gathered
-        start_year = input('optional - Search Start year (Press enter to set as earliest available date, May 1, 2009):')
-        valid_start_year = validate_start_date(start_year,end_year_input, earliest_date)
+        start_year = input('Start date invalid. optional - Search Start year (Press enter to set as earliest available date, May 1, 2009):')
+        valid_start_year = validate_start_date(start_year, end_year_input, earliest_date)
 
     valid_end_year = validate_end_date(valid_start_year,end_year_input, earliest_date)
     while valid_end_year == False:
-        end_year_input = input('optional - Search end year (Press enter to set as latest available date):')
+        end_year_input = input('End date invalid. optional - Search end year (Press enter to set as latest available date):')
         valid_end_year = validate_end_date(valid_start_year,end_year_input, earliest_date) #stores either False or a valid_end_year
 
+    print('Your date range is:', valid_start_year,'-', valid_end_year)
     return [valid_start_year, valid_end_year]
 
 #get the file type
 #so far only returns '10-k'
 #returns true or false
 def get_file_type():
-    filetype = input ('What document are you looking for?')
-    while not re.match('10[ -]?[kK]', filetype):
-        filetype = input ('Invalid document.\n\nWhat document are you looking for?')
-    else:
-        return '10-K'
-      
+    filetype = input ('What document are you looking for? (press \'Enter\' for 10-K. Only 10-K supported)')
+    print(f'You entered _{filetype}_')
+    while True:
+        if re.match('10[ -]?[kK]', filetype) or filetype == '':
+            return '10-K'
+        else:
+            filetype = input ('Invalid document.\n\nWhat document are you looking for?')
+
 #returns a page worth of html
 def get_files(CIK, search_range,file_type):
 
@@ -164,6 +187,9 @@ def get_files(CIK, search_range,file_type):
     #get html content
     page = requests.get(link)
     soup = BeautifulSoup(page.content, 'html.parser')
+
+    print ('Html retrieved')
+
     return soup
 
 
@@ -171,7 +197,7 @@ def get_files(CIK, search_range,file_type):
 #returns a dataframe
 def get_files_df(soup,search_range):
     file_rows = list(soup.find_all('tr'))[3:]
-
+    print('File rows obtained', file_rows)
     #create list that contain each file's information
     f_types =[]
     f_links =[]
@@ -182,21 +208,25 @@ def get_files_df(soup,search_range):
     for row in file_rows:
         #gets file date
         file_type = row.find_all('td',{'nowrap':'nowrap'})[0].text
+        print('file_type',file_type)
         file_date = row.find_all('td')[3].text.replace('-','')
+        print('file_date',file_date)
         file_link = sec_link+row.find_all('td',{'nowrap':'nowrap'})[1].find_all('a', href = True)[0]['href']
+        print('file_link',file_link)
 
         #this gets every item's file link
-
         if int(file_date) > int(search_range[0]) and int(file_date)<int(search_range[1]) and file_type == '10-K':
-
             f_types.append(file_type)
             f_dates.append(file_date)
             f_links.append(file_link)
-
+    print('f_types', f_types)
+    print('f_links', f_links)
+    print('f_dates', f_dates)
 
     #construct a table:
     #['file type']: 10-k | ['file link']: sec.gov/Archive/....| ['file date': 19960303]
     files_table = pd.DataFrame({'file type':f_types,'file link':f_links,'file date':f_dates})
+    print('Data imported to table', files_table)
     return files_table
 
 
@@ -217,14 +247,17 @@ def filtered_df(search_range,file_type,files_table):
 def get_list_doc_link_text(filtered_df):
     sec_link = 'http://sec.gov'
     file_links = filtered_df['file link'].tolist()
+    print('file_links', file_links)
     doc_links = []
     for URL in file_links:
         page_specific_10k = requests.get(URL)
         soup = BeautifulSoup(page_specific_10k.content, 'html.parser')
-        complete_file_link = soup.find_all('table')[0].find_all('tr')[-1].\
-                                        find_all('a',href = True)[0]['href']
+        complete_file_link = soup.find_all('table')[0].find_all('tr')[-1].find_all('a',href = True)[0]['href']
         doc_full_link = sec_link + complete_file_link
+        print(doc_full_link) #DEBUG
         doc_links.append(doc_full_link)
+    print('Links obtained')
+    print(doc_links) #DEBUG
     return doc_links
 
 def get_link(filtered_df):
@@ -232,19 +265,19 @@ def get_link(filtered_df):
 
 #No actual return just print caluclation
 def calculation(links):
-    print('\n')
-    print('\n')
+    print('Opening links')
     for link in links:
+        print(link)
         #read the txt from link
         broken_xml = urllib.request.urlopen(link).read().decode('utf-8')
 
         #break the thing into pages
         pages = broken_xml.split('page-break')
-        #list the documents that we are looking for 
+        #list the documents that we are looking for
         documents = ['Consolidated Statements of Cash Flows','Consolidated Statements of Operations', 'Consolidated Statements of Comprehensive Income', 'Consolidated Balance Sheets', 'CONSOLIDATED STATEMENTS OF STOCKHOLDERS']
-        
+
         #if the name of statements are found in any of the page
-        #put the found statement's name and the text content into a list 
+        #put the found statement's name and the text content into a list
 
         doc_list = []
         for page in pages:
@@ -253,9 +286,10 @@ def calculation(links):
             for i in documents:
                 if i.upper() in page:
                     key = i
-                    found = +1
+                    found += 1
+                print('Found', i)
 
-            if found>=1:
+            if found >= 1:
                 pair = [key,page]
                 doc_list.append(pair)
 
@@ -263,6 +297,7 @@ def calculation(links):
         file = open('test.html','w')
         for i in doc_list:
             file.write(i[1])
+        print('Test html exported')
         file.close()
 
         #create a list called statements to include the following
@@ -339,7 +374,7 @@ def calculation(links):
         #set variables used for calculation
         revenue = 0
         COGS = 0
-        GrossProfit= 0 
+        GrossProfit= 0
         OPEX = 0
         DEPEX = 0
         tax = 0
@@ -414,11 +449,10 @@ def main():
     search_range = get_years() #date range
     file_type = get_file_type()
     initial_search_html = get_files(comp_name,search_range,file_type)
-    file_link_df = get_files_df(initial_search_html,search_range)
-    filtered = filtered_df(search_range,file_type,file_link_df) #QUESTION: function not used
+    file_link_df = get_files_df(initial_search_html,search_range) #returns files table
+    filtered = filtered_df(search_range,file_type,file_link_df) #temporarily unused function. passes through files table
     files_list_links = get_list_doc_link_text(filtered)
-    print('\n')
-    print('\n')
+    print('\n\n')
     calculation(files_list_links)
     while True:
         user = input('Would you like to do another search? (y\\n)')
